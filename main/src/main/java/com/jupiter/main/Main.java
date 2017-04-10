d49d7d91af2fece8b350bf519dafa8e4fd7f2528
Zheng Zhang(张政) <zheng.zhang@msxf.com>
2017-04-10 17:12:57 +0800

    update

---
+++ main/src/main/java/com/jupiter/main/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.jupiter.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zheng.zhang
+ * 
+ *         2017年4月10日
+ */
+
+@SpringBootApplication
+//@EnableAutoConfiguration
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
